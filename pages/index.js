8da65177d70df3639a6583d12d748de3b6286a24
b1 <112481563+b1ai@users.noreply.github.com>
2022-09-14 14:49:37 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,47 @@
-import Link from 'next/link';
-import { getPosts } from '../utils/mdx-utils';
-
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import Layout, { GradientBackground } from '../components/Layout';
-import ArrowIcon from '../components/ArrowIcon';
-import { getGlobalData } from '../utils/global-data';
-import SEO from '../components/SEO';
-
-export default function Index({ posts, globalData }) {
-  return (
-    <Layout>
-      <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.name} />
-      <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">
-          {globalData.blogTitle}
-        </h1>
-        <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
-              <Link
-                as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-                href={`/posts/[slug]`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      {post.data.date}
-                    </p>
-                  )}
-                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-                  {post.data.description && (
-                    <p className="mt-3 text-lg opacity-60">
-                      {post.data.description}
-                    </p>
-                  )}
-                  <ArrowIcon className="mt-4" />
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-      <Footer copyrightText={globalData.footerText} />
-      <GradientBackground
-        variant="large"
-        className="fixed top-20 opacity-40 dark:opacity-60"
-      />
-      <GradientBackground
-        variant="small"
-        className="absolute bottom-0 opacity-20 dark:opacity-10"
-      />
-    </Layout>
-  );
-}
-
-export function getStaticProps() {
-  const posts = getPosts();
-  const globalData = getGlobalData();
-
-  return { props: { posts, globalData } };
-}
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>b1 | c.sly.io</title>
+    <link href="/style.css" rel="stylesheet" type="text/css" media="all">
+  </head>
+  <body>
+  <div align="center">
+    <br>
+    <a href="https://b1.neocities.org/"><img src="/b1.jpg" width="100" height="81"></a>
+    <p><span style="font-size:40px"><strong>b1</strong></span></p>
+    <p><span style="font-size:24px"><strong><a href="https://b1.neocities.org/">c.sly.io</a></strong></span></p>
+    <p><span style="font-size:16px"><strong>Webdesign | Applications</strong></span></p>
+    <p><span style="font-size:16px"><strong>OnlineShops | Marketplaces</strong></span></p>
+    <p><span style="font-size:16px"><strong>Photography | Videography</strong></span></p>
+    <p><span style="font-size:16px"><strong>Social Media Optimization</strong></span></p>
+    <p><span style="font-size:24px"><strong><a href="https://vvv.code.blog" target="_blank">vvv.code.blog</a></strong></span></p>
+    <p><span style="font-size:24px"><strong><a href="https://g.dev/maps" target="_blank">Google Developer</a></strong></span></p>
+    <p><span style="font-size:24px"><strong><a href="https://devs.typedream.app/" target="_blank">Devs Typedream</a></strong></span></p>
+    <p><span style="font-size:24px"><strong><a href="https://pagespeed.web.dev/report?url=https://b1.neocities.org" target="_blank">SuperFast</a></strong></span></p>
+    <br>
+    <h2>Contact</h2>
+    <br>
+    <p><span style="font-size:36px"><strong><a href="tel:+31687900332">Call</a></strong></span></p>
+    <br>
+    <p><span style="font-size:36px"><strong><a href="https://wa.me/message/JIWJISLKSAKQG1" target="_blank">WhatsApp</a></strong></span></p>
+    <br>
+    <p><span style="font-size:36px"><strong><a href="mailto:michael.weber.x@gmail.com">Mail</a></strong></span></p>
+    <br>
+    <h2>Find</h2>
+    <br>
+    <p><a href="https://g.co/kgs/bd74QM" target="_blank"><img src="/google-maps.png" width="100" height="100"></a></p>
+    <br>
+    <details>
+      <summary><span style="font-size:20px"><strong>Legal</strong></span></p></summary>
+       <p><strong>Michael Weber | CEO</strong></p>
+       <p>Görlitzer Straße 3</p>
+       <p>10997 Berlin | Germany</p>
+       <p>Phone +31(6)87900332</p>
+       <p>URL <a href="https://b1.neocities.org/">b1.neocities.org</a></p>
+       <p>Short URL <a href="https://b1.neocities.org/">c.sly.io</a></p>
+    </details>
+    <p>Copyright © 2022 b1</p>
+    </div>
+  </body>
+</html>
